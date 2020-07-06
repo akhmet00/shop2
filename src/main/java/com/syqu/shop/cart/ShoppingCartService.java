751dc3225ccb3386ee25cf1f6f413e7dd66f956d
akhmet00 <akhmet00@mail.ru>
2020-07-06 14:31:55 +0600

    Initial commit

---
+++ src/main/java/com/syqu/shop/cart/ShoppingCartService.java
@@ -0,0 +1,17 @@
+package com.syqu.shop.cart;
+
+import com.syqu.shop.product.Product;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+public interface ShoppingCartService {
+    void addProduct(Product product);
+    void removeProduct(Product product);
+    void clearProducts();
+    Map<Product, Integer> productsInCart();
+    BigDecimal totalPrice();
+    void cartCheckout();
+}
---
+++ src/main/java/com/syqu/shop/user/UserService.java
@@ -0,0 +1,9 @@
+package com.syqu.shop.user;
+
+public interface UserService {
+    void save(User user);
+    void login(String username, String password);
+    User findByUsername(String username);
+    User findByEmail(String email);
+    User findById(long id);
+}
